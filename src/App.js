e1777867eb7ff49aa74a446cc1adea6d6d987b8c
ariqsyahalam <ariq.syahalam@gmail.com>
2021-02-27 17:03:47 +0700

    header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import "./App.css";
 import { Button } from "./component/button";
 import { ActivityCard } from "./component/activityCard";
-
+import { HeaderTitle } from "./component/header";
 
 function App() {
   return (
     <div className="App">
+      <HeaderTitle />
       <Button text="New Task" />
       <Button text="Task List" />
       <p>test</p>
-      <ActivityCard title="Activity #1" date="February"/>
+      <ActivityCard title="Activity #1" date="February" />
     </div>
   );
 }
---
+++ src/component/header.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+import bookmark from "./assets/bookmark.png";
+
+export const HeaderTitle = () => {
+
+  return (
+    <>
+      <Styled>
+        <div className="header-wrapper">
+          <p className="title">Todo List</p>
+          <img src={bookmark} alt="icon" className="icon" />
+        </div>
+      </Styled>
+    </>
+  );
+};
+
+export const Styled = styled.div`
+.header-wrapper{
+    background-color: #13293D;
+    display: flex;
+    
+    .title {
+        font-size: 96px;
+        line-height: 120px;
+        font-style: normal;
+        font-weight: bold;
+        color: white;
+        margin: 0;
+        padding-top: 89px;
+    }
+    .icon{
+        width: 66.5px;
+        height: 76px;
+        margin-top: 116px;
+        padding-left: 32px;
+    }
+}
+
+`;
